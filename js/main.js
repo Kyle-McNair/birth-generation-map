d238b96245ba1a6cf839361f8849779786fde89b
Kyle-McNair <58085298+Kyle-McNair@users.noreply.github.com>
2020-06-21 14:18:47 -0500

    Proportional Symbols in scrolling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-
-setMap()
+function whole(){
 var main = d3.select("body");
 var scrolly = main.select("#scrolly");
 var figure = scrolly.select("figure");
@@ -7,6 +6,8 @@ var article = scrolly.select("article");
 var stepper = article.selectAll(".stepper");
 const scroller = scrollama();
 const stickyscroller = scrollama()
+var map, albers, msa2018, height, width;
+console.log(map)
 function handleStepEnter(response){
     if(response.direction === 'down'){
         response.element.style.opacity = 1
@@ -15,12 +16,7 @@ function handleStepEnter(response){
         response.element.style.opacity = 0
     }
 }
-function handleStickyEnter(response){
-    stepper.classed("is-active", function(d, i) {
-        return i === response.index;
-      });
-      figure.select("p").text(response.index + 1);
-}
+
 function handleStepExit(response){
     if(response.direction === 'up'){
         response.element.style.opacity = 0
@@ -34,15 +30,9 @@ function setupStickyfill() {
       Stickyfill.add(this);
     });
   }
+
 function init(){
-setupStickyfill();
-stickyscroller
-    .setup({
-        step: '#scrolly article .stepper',
-        debug: true,
-        offset: 0.7
-    })
-    .onStepEnter(handleStickyEnter)
+
 scroller
     .setup({
         step: '.step',
@@ -53,89 +43,112 @@ scroller
     .onStepExit(handleStepExit);
     window.addEventListener("resize", scroller.resize);
 }
-init();
-// load proportional symbol map //
-function setMap(){
-    var height = screen.height*0.9,
-    width = screen.width;
 
-    //create new svg container for the map
-    var map = d3.select("figure.prop-map")
-        .append("svg")
-        .attr("viewBox", `0 0 ${width} ${height}`)
-        .attr("width", "100%")
-        .attr("height", "100%")
+// load proportional symbol map //
+height = screen.height*0.9,
+width = screen.width;
+
+ //create new svg container for the map
+map = d3.select("figure.prop-map")
+    .append("svg")
+    .attr("class","map")
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .attr("width", "100%")
+    .attr("height", "100%")
         // .style("position","absolute");
 
-    //create Albers equal area conic projection centered on Chicago
-    var albers = d3.geoAlbers()
-        .scale(width)
-        .translate([width / 2, height / 2]);
+// prop = map.append("g")
+//     .attr("opacity",1)
+
+console.log(map)
+albers = d3.geoAlbers()
+    .scale(width)
+    .translate([width / 2, height / 2]);
 
     //call in the projection
-    var path = d3.geoPath()
-        .projection(albers);
+var path = d3.geoPath()
+    .projection(albers);
 
     //use Promise.all to parallelize asynchronous data loading
-    var promises = [];
+var promises = [];
 
-    promises.push(d3.json("data/topos/states.topojson"));
-    promises.push(d3.json("data/msa_2018_geojson.json"));
+promises.push(d3.json("data/topos/states.topojson"));
+promises.push(d3.json("data/msa_2018_geojson.json"));
     //list of promises goes and has the callback function be called
-    Promise.all(promises).then(callback);
+Promise.all(promises).then(callback);
     
-    function callback(data){
-        states = data[0]
-        msa2018 = data[1]
+function callback(data){
+    states = data[0]
+    msa2018 = data[1]
+    console.log(msa2018)
+    var country = topojson.feature(states, states.objects.states);
+    console.log(map)
+    var states_US = map.append("path")
+        .datum(country)
+        .attr("class", "states")
+        .attr("d", path)
+        .attr("stroke", "#FFFFFF")
+        .attr("stroke-width", "0.25px")
+        .attr("fill", "#111111");
+    
+    setupStickyfill();
+    stickyscroller
+    .setup({
+        step: '#scrolly article .stepper',
+        debug: true,
+        offset: 0.85
+    })
+    .onStepEnter(updatePropSymbols)
 
-        var country = topojson.feature(states, states.objects.states);
-        // var msa_2018 = topojson.feature(msa2018, msa2018.objects.msa_2018_geojson)
-        
-        console.log(country)
-        console.log(msa2018)
-        var states_US = map.append("path")
-            //calls the stateRegions from above
-            .datum(country)
-            //states class used to change color in css
-            .attr("class", "states")
-            .attr("d", path)
-            .attr("stroke", "#FFFFFF")
-            .attr("stroke-width", "0.25px")
-            .attr("fill", "#111111")
-            .transition()
-                .duration(2500)
-                .ease(d3.easeLinear);
+} 
+function updatePropSymbols(response){
+    map.selectAll(".proportional")
+    .remove()
+    var colors = ["#7b3393","#c2a5cf","#d2eadb","#a7d5a0","#078844"];
+    var gen = ["SilentG","BabyBoomer","GenX","Millennials","GenZ"];
+    var density = ["SilentG_Density","BabyBoomers_Density","GenX_Density","Millennials_Density","GenZ_Density"]
 
-
-        var totals = [];
-        for (var i in msa2018.features) {
-            var ratio = msa2018.features[i].properties.Millennials_Density;
-            totals.push(Number(ratio));
-        }
-        
-        var min = Math.min.apply(Math, totals);
-        var max = Math.max.apply(Math, totals);
-
-        console.log(min, max)
-        var radius = d3.scaleSqrt()
-            .domain([0, max])
-            .range([1, 10*(width/700)]);
-
-
-        var props = map.append("g")
-            .attr("class","proportionalSymbols")
-            .selectAll(".proportional")
-            .data(msa2018.features)
-            .enter()
-            .append("circle")
-                .attr("class","proportional")
-                .attr("cx", function(d){return albers(d.geometry.coordinates)[0]})
-                .attr("cy", function(d){return albers(d.geometry.coordinates)[1]})
-                .attr("r", function(d){return radius(d.properties.Millennials_Density)})
-                .attr("fill", "#a7d5a0")
-                .attr("fill-opacity", 0.6)
-                .attr("stroke", "#a7d5a0")
-                .attr("stroke-width", 0.2);
- 
+    var index = response.index
+    var totals = [];
+    for (var i in msa2018.features) {
+        var r1 = msa2018.features[i].properties.SilentG_Density
+        var r2 = msa2018.features[i].properties.BabyBoomers_Density
+        var r3 = msa2018.features[i].properties.GenX_Density
+        var r4 = msa2018.features[i].properties.Millennials_Density
+        var r5 = msa2018.features[i].properties.GenZ_Density
+        totals.push(Number(r1))
+        totals.push(Number(r2))
+        totals.push(Number(r3))
+        totals.push(Number(r4))
+        totals.push(Number(r5))
     }
+    
+    var minRadius = 0.25
+        
+    var min = Math.min.apply(Math, totals);
+    var max = Math.max.apply(Math, totals);
+    console.log(max)
+    var radius = d3.scaleSqrt()
+        .domain([min, max])
+        .range([1, 15*(width/700)]);
+
+    map
+    .selectAll('.map')
+    .data(msa2018.features)
+    .enter()
+    .append("circle")
+        .attr("class","proportional")
+        .attr("cx", function(d){return albers(d.geometry.coordinates)[0]})
+        .attr("cy", function(d){return albers(d.geometry.coordinates)[1]})
+        .transition()
+        .duration(750)
+            .attr("r", function(d){return 1.0083 * Math.pow(d.properties[density[index]]/min,0.5715) * minRadius})
+        .attr("fill", colors[index])
+        .attr("fill-opacity", 0.6)
+        .attr("stroke", colors[index])
+        .attr("stroke-width", 0.2);
+    
 }
+init()
+}
+$(document).ready(whole);
