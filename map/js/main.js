cca6dfdb43e8064a3d5d54c94eee0acfb713fb15
Kyle-McNair <58085298+Kyle-McNair@users.noreply.github.com>
2020-06-22 22:15:48 -0500

    map template creation

---
+++ map/js/main.js
@@ -0,0 +1,19 @@
+
+var map;
+function setMap(){
+
+    var dark = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: 'abcd',
+        minZoom: 0,
+        maxZoom: 20,
+        ext: 'png'
+    });
+
+    map = L.map('map', {
+        center: [38,-98],
+        zoom: 5,
+		layers: [dark]
+	});
+}
+$(document).ready(setMap);
